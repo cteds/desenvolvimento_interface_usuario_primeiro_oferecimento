61db0de9680dd0eef346f317aad4a63adc99914e
Saulo <saulo.macedoprojetos@gmail.com>
2022-09-23 21:58:20 -0300

    Adiciona conteudo aula 2 - crud com sqlite

---
+++ aula 2 - CRUD com SQLite/wpf_catalog/Data/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace wpf_catalog.Data;
+
+public class Product
+{
+    public Guid Id { get; set; }
+
+    public string SKU { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public double Price { get; set; }
+}
---
+++ aula 2 - CRUD com SQLite/wpf_catalog/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Windows;
+using wpf_catalog.Data;
+
+namespace wpf_catalog;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly Context context;
+    Product NewProduct = new();
+    Product selectedProduct = new();
+
+    public MainWindow(Context context)
+    {
+        this.context = context;
+        InitializeComponent();
+        GetProducts();
+        NewProductGrid.DataContext = NewProduct;
+    }
+
+    private void GetProducts()
+    {
+        ProductDataGrid.ItemsSource = context.Products.ToList();
+    }
+
+    private void AddItem(object s, RoutedEventArgs e)
+    {
+        context.Products.Add(NewProduct);
+        context.SaveChanges();
+        GetProducts();
+        NewProduct = new Product();
+        NewProductGrid.DataContext = NewProduct;
+    }
+
+    private void UpdateItem(object s, RoutedEventArgs e)
+    {
+        context.Update(selectedProduct);
+        context.SaveChanges();
+        GetProducts();
+        UpdateProductGrid.DataContext = null;
+    }
+
+    private void SelectProductToEdit(object s, RoutedEventArgs e)
+    {
+        selectedProduct = (s as FrameworkElement).DataContext as Product;
+        UpdateProductGrid.DataContext = selectedProduct;
+    }
+
+    private void DeleteProduct(object s, RoutedEventArgs e)
+    {
+        var productToDelete = (s as FrameworkElement).DataContext as Product;
+        context.Products.Remove(productToDelete);
+        context.SaveChanges();
+        GetProducts();
+    }
+}
